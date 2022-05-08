76910a8525f190e98bfea638a069305d29e21521
Subodh Joshi <s.joshi2705@gmail.com>
2022-05-08 23:02:25 +0530

    second commit

--- dp/lcs/LCS.cpp
+++ dp/lcs/LCS.cpp
@@ -34,6 +34,29 @@ int lcs1(char* X,char* Y,int n,int m,vector<vector<int>>&dp)
     
     
 }
+
+//tabulation
+int lcs2(char* X,char* Y,int n,int m){
+    int dp[n+1][m+1];
+    for (int i = 0; i < n+1; i++)
+    {
+        for (int j = 0; j < m+1; j++)
+        {
+            if(i==0 || j==0)
+                dp[i][j]=0;
+            else if(X[i-1]==Y[j-1])
+            {
+                dp[i][j] = dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+    }
+    return dp[n][m];
+    
+}
 int main()
 {
      char X[] = "AGGTAB";
@@ -41,7 +64,9 @@ int main()
      
     int n= strlen(X);
     int m = strlen(Y);
-    vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-    cout<<lcs1(X,Y,n,m,dp)<<endl;
-    cout<<LCS(X,Y,n,m);
+    // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    // cout<<lcs1(X,Y,n,m,dp)<<endl;
+    // cout<<LCS(X,Y,n,m);
+    cout<<lcs2(X,Y,n,m);
+
 }
