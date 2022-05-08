d3aa55d43acf4f365a1c0d3dc66ad3cf62f1ad16
Subodh Joshi <s.joshi2705@gmail.com>
2022-05-08 22:43:12 +0530

    second commit

--- dp/lcs/LCS.cpp
+++ dp/lcs/LCS.cpp
@@ -17,6 +17,23 @@ int LCS(char* X,char* Y,int n,int m)
     }
 }
 
+//memoization
+int lcs1(char* X,char* Y,int n,int m,vector<vector<int>>&dp)
+{
+    if(n==0 || m==0){
+        return 0;
+    }
+    if (dp[n][m]!=-1)
+    {
+        return dp[n][m];
+    }
+    if(X[n-1]==Y[m-1])
+        return dp[n][m] = 1+lcs1(X,Y,n-1,m-1,dp);
+    
+    return dp[n][m] = max(lcs1(X,Y,n-1,m,dp) ,lcs1(X,Y,n,m-1,dp));
+    
+    
+}
 int main()
 {
      char X[] = "AGGTAB";
@@ -24,6 +41,7 @@ int main()
      
     int n= strlen(X);
     int m = strlen(Y);
-
+    vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    cout<<lcs1(X,Y,n,m,dp)<<endl;
     cout<<LCS(X,Y,n,m);
 }
